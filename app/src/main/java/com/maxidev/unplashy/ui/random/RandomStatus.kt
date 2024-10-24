913a3e1460e1991ba084ddc18d76394172b5841b
Maxi Pelizzoni <maxi96p@gmail.com>
2024-10-24 17:04:20 -0300

    Random feature deleted.

--- app/src/main/java/com/maxidev/unplashy/ui/random/RandomStatus.kt
+++
@@ -1,8 +0,0 @@
-package com.maxidev.unplashy.ui.random
-
-import com.maxidev.unplashy.domain.model.RandomPhoto
-
-sealed interface RandomStatus {
-    data class Success(val onSuccess: RandomPhoto? = null): RandomStatus
-    data class Error(val onError: Exception): RandomStatus
-}
