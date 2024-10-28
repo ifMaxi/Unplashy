ef9824270e7af31c8d1db478167c1bff038d75f9
Maxi Pelizzoni <maxi96p@gmail.com>
2024-10-28 17:27:51 -0300

    Offline cache implemented.

--- app/src/main/java/com/maxidev/unplashy/di/RemoteModule.kt
+++ app/src/main/java/com/maxidev/unplashy/di/RemoteModule.kt
@@ -1,16 +1,20 @@
 package com.maxidev.unplashy.di
 
+import android.content.Context
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import com.maxidev.unplashy.data.remote.ApiKeyInterceptor
 import com.maxidev.unplashy.data.remote.CacheInterceptor
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import kotlinx.serialization.json.Json
+import okhttp3.Cache
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import java.io.File
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
@@ -24,10 +28,17 @@ object RemoteModule {
 
     @Provides
     @Singleton
-    fun providesRetrofit(): Retrofit {
+    fun providesRetrofit(
+        @ApplicationContext context: Context
+    ): Retrofit {
 
         val json = Json { ignoreUnknownKeys = true }
+        val cache = Cache(
+            directory = File(context.cacheDir, "cache"),
+            maxSize = 50L * 1024L * 1024L
+        )
         val okHttpClient = OkHttpClient.Builder()
+            .cache(cache)
             .readTimeout(TIMEOUT, TimeUnit.SECONDS)
             .connectTimeout(TIMEOUT, TimeUnit.SECONDS)
             .addInterceptor(ApiKeyInterceptor())
